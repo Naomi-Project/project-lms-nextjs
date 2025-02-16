2a7716a7784ae929beec1a4006295205a88df8ef
Ahmad Syaifudin <sysfdn@pm.me>
2025-02-17 02:58:54 +0700

    fix: use secure

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -2,22 +2,22 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-    uri: 'http://staging.zerobase.tech/graphql',
+  uri: 'https://staging.zerobase.tech/graphql',
 })
 
 const authLink = setContext((_, { headers }) => {
-    const token = localStorage.getItem('authToken');  
-    return {
-      headers: {
-        ...headers,
-        authorization: token ? `Bearer ${token}` : "",
-      }
+  const token = localStorage.getItem('authToken');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
     }
-  });
+  }
+});
 
-  const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache()
-  });
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
 export default client;
