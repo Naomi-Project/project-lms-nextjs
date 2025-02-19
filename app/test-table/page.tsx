9e7ede3f6ffb9b00587e6ca8b30c185cac636cc8
zeru-program <justinebogor0609@gmail.com>
2025-02-19 20:49:00 +0700

    feat: implement data table to list table data

--- app/test-table/page.tsx
+++ app/test-table/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+"use client"
+import { DataTable } from "@/components/common/list/CommonDataTable"
 
-const TestTable = () => {
-  return (
-    <div>page</div>
-  )
+type Payment = {
+  id: string
+  username: string
+  amount: number
+  status: "pending" | "processing" | "success" | "failed"
+  email: string
 }
 
-export default TestTable
+const paymentData: Payment[] = [
+  { id: "1", amount: 100, username: "abdul", status: "success", email: "user1@example.com" },
+  { id: "2", amount: 200, username: "budi", status: "pending", email: "user2@example.com" },
+]
+
+const paymentColumns = [
+  { accessorKey: "username", header: "Email", cell: ({ row }: any) => <span>{row.getValue("email")}</span> },
+  { accessorKey: "email", header: "Email", cell: ({ row }: any) => <span>{row.getValue("email")}</span> },
+  { accessorKey: "amount", header: "Amount", cell: ({ row }: any) => <span>{row.getValue("amount")}</span> },
+]
+
+export default function PaymentTable() {
+  return <DataTable columns={paymentColumns} data={paymentData} filterName="usernmae" filterPlaceholder="Filter by email..." />
+}
